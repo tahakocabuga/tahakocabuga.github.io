db74008d5c10971be97a9670dfa6be20ec77f075
Taha Kocabuga <tahakocabuga@gmail.com>
2022-02-22 12:07:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+console.log("Hello!");
+
+// document.getElementsByClassName("div-banners").forEach(el => {
+//  let resWidth = document.getElementById('div').width
+//  el.width = document.getElementById('div').width;
+// })
+
+let banners = document.getElementsByClassName("div-banners");
+let resWidth = document.getElementById("div").width;
+for (let i = 0; i < banners.length; i++) {
+  banners[i].style.width = resWidth;
+}
+
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #15f4ee}";
+  document.body.appendChild(css);
+};
+
+function light() {
+  document.body.style.color = "black";
+  document.body.style.backgroundColor = "white";
+  document.getElementById("")
+}
